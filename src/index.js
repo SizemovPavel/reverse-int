5ea8724b27b6d693d13c0f4ce05568ec649c515c
SizemovPavel <sizemovpavel@gmail.com>
2024-07-29 22:34:36 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const absoluteNum = Math.abs(n);
+    const strOfnumber = absoluteNum.toString();
+    return +strOfnumber.split('').reverse().join('');
 }
